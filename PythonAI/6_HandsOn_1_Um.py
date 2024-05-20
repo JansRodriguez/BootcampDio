c13082088f33e89919bf7c6f830b157b8d32eed2
Janes Rodrigues <engaero.jns@gmail.com>
2024-05-20 16:19:23 -0300

    Desafio 01 realizado

--- PythonAI/6_HandsOn_1_Um.py
+++ PythonAI/6_HandsOn_1_Um.py
@@ -7,19 +7,52 @@ menu = """
 """
     ## VARIÁVEIS
 saldo = 0
-limite = 500
+limite_valor_sacado = 500
 numero_saques = 0
+numero_deposito = 0
+extrato = ""
 LIMITES_SAQUES = 3
 
 while True:
-    opcao = int(input(menu))
+    
+    print(menu)
+    opcao = int(input("Digite sua opcao: "))
 
     if opcao == 1:
-        print("Depositar")
+        depósito = float(input('Informe o valor que desejar depostar: '))
+
+        if depósito <= 0:
+            print('Operação inválida. É necessário depositar valores superior a R$ 0,01')
+        else:
+            numero_deposito += 1
+            saldo += depósito
+            extrato += f'Depósito: R$ {saldo:.2f}\n'
+
     elif opcao == 2:
-        print("Sacar")
+        saque = float(input('Informe o valor a sacar: '))
+        
+        excedeuLimiteSaques = numero_saques >= LIMITES_SAQUES
+
+        if saque > saldo:   ## saldo_insuficiente
+            print('Você não possui saldo suficiente para a operação.')
+        elif saque > limite_valor_sacado:   ## excedeuLimite
+            print('O limite diário para a operação de saque foi excedido.')
+        elif excedeuLimiteSaques:
+            print('Você já realizou o máximo de operações diárias de saques.')
+        elif saque > 0:
+            numero_saques += 1
+            saldo -= saque
+            extrato += f'Saque: R$ {saque:.2f}\n'
+            print('Saque realizado com sucesso!!!')
+
     elif opcao == 3:
-        print("Extrato")
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+        print(f'Saldo da conta: R$ {saldo}\nQuantidade de depósito realizado: {numero_deposito}\nQuantidade de saques realizados: {numero_saques}')
+
     elif opcao == 4:
         break
     else:
