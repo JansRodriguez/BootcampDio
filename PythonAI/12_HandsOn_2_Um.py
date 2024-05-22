b63b9e7ce2813f7d229de193e347909f87ed1fa5
Janes Rodrigues <engaero.jns@gmail.com>
2024-05-22 11:41:03 -0300

    Realizando atualização na entrega do segundo desafio do Bootcamp. Desafio pesado.

--- PythonAI/12_HandsOn_2_Um.py
+++ PythonAI/12_HandsOn_2_Um.py
@@ -3,16 +3,37 @@
         CRIAR DUAS NOVAS FUNÇÕES: CADASTRAR USUÁRIO (CLIEMTE) E CADASTRAR CONTA BANCÁRIA
     """
 def menu():
-    menu = """\n
+    menu = """
     ================ MENU ================
-    [d]  - Depositar
-    [s]  - Sacar
-    [e]  - Extrato
-    [nc] - Nova conta
-    [lc] - Listar contas
-    [nu] - Novo usuário
-    [q]  - Sair
-    => """
+    [1]  -  Depositar
+    [2]  -  Sacar
+    [3]  -  Extrato
+    [4]  -  Nova conta
+    [5]  -  Listar contas
+    [6]  -  Novo usuário
+    [0]  -  Sair
+    """
     return input(menu)
 
-menu()
+## FUNÇÃO SAQUE - KEYWORDS ONLY
+def saque(*, saldo, valorSaque, extrato, limiteValor, numeroDeSaques, limiteDeSaques):
+
+    saldoInsuficiente = saldo < valorSaque
+    excedeuLimite = limiteValor < valorSaque
+    excedeuSaques = limiteDeSaques <= numeroDeSaques
+
+    if saldoInsuficiente:
+        print(' ⚠️ ⚠️ ⚠️ Você não possui saldo suficiente para a operação.')
+    elif excedeuLimite:
+        print('⚠️ ⚠️ ⚠️ O limite diário para a operação de saque foi excedido.')
+    elif excedeuSaques:
+        print('⚠️ ⚠️ ⚠️ Você já realizou o máximo de operações diárias de saques.')
+    elif saldo > 0:
+        saldo -= valorSaque
+        numeroDeSaques += 1
+        extrato += f'Saque: R$ {valorSaque:.2f}'
+        print('Saque realizado com sucesso!!!')
+    else:
+        print('Operação inválida. É necessário depositar valores superior a R$ 0,01')
+        
+    return saldo, extrato
