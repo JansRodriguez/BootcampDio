39b6bb400139f2500cb483b04c5f3f8b0c93345a
Janes Rodrigues <engaero.jns@gmail.com>
2024-05-22 17:46:07 -0300

    Realizando atualização na entrega do segundo desafio do Bootcamp. Desafio pesado.

--- PythonAI/12_HandsOn_2_Um.py
+++ PythonAI/12_HandsOn_2_Um.py
@@ -35,5 +35,77 @@ def saque(*, saldo, valorSaque, extrato, limiteValor, numeroDeSaques, limiteDeSa
         print('Saque realizado com sucesso!!!')
     else:
         print('Operação inválida. É necessário depositar valores superior a R$ 0,01')
-        
+
     return saldo, extrato
+## FUNÇÃO DEPÓSITO - POSITIONAL ONLY
+def deposito(saldo, valorDeposito, extrato, numeroDeposito, /):
+
+    if valorDeposito <= 0:
+        print(f'O valor R$ {valorDeposito} não é aceitável para depósito.\nPor gentileza, informar um valor válido')
+    elif valorDeposito > 0:
+        saldo += valorDeposito
+        numeroDeposito += 1
+        extrato += f'Depósito R$ {saldo:.2f}\n'
+        print('Depósito realizado com sucesso!!!')
+
+    return saldo, extrato
+## FUNÇÃO EXTRATO - POSITIONAL ONLY E KEYWORD ONLY
+    ## ARGUMENTOS POSIONAIS: saldo, 
+    ## ARGUMENTOS NOMEADOS: extrato
+def extrato(numeroDeposito, numeroDeSaques, saldo, /, *, extrato):
+    print("================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+
+    print("==========================================")
+    print(f'Saldo da conta: R$ {saldo}\nQuantidade de depósito realizado: {numeroDeposito}\nQuantidade de saques realizados: {numeroDeSaques}')
+## FUNÇÃO PARA VERIFICAR SE EXISTE O USUÁRIO ANTES DE CRIAR USUÁRIOS
+def verificarUsuário(cpf, usuarios):
+    usuáriosVerificados = [usuario for usuario in usuarios if usuario['cpf'] == cpf]
+    return usuáriosVerificados[0] if usuáriosVerificados else 'Usuário ainda não cadastrado.'
+## FUNÇÃO CRIAR USUÁRIOS
+def criarUsuarios(usuarios):
+    cpf = input("Informe o numero do CPF: ")
+    usuario = verificarUsuário(cpf, usuarios)
+
+    if usuario:
+        print('Já existe usuário coom esse CPF!!')
+        return
+    
+    nome = input('Informe o noome completo: ')
+    dataNascimento = input('Informe a data de nascimento (dd-mm-aaaa): ')
+    endereco = input("Informe o endereço completo (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({
+        'Nome': nome,
+        'Data de Nascimento': dataNascimento,
+        'Endereço' : endereco
+    }
+    )
+
+    print('Usuário cadastrado com sucesso!!!')
+## FUNÇÃO PARA CRIAR CONTAS
+def criarContas(agencia, numeroDaConta, usuarios):
+    cpf = input("Informe o numero do CPF: ")
+    usuario = verificarUsuário(cpf, usuarios)
+
+    if usuario:
+        print('Conta criada com sucesso!!!')
+        return {
+            "Agencia" : agencia,
+            "Número da Conta" : numeroDaConta,
+            "Usuários" : usuarios
+        } 
+    print('Usuário não encontrado!!!')
+## FUNÇÃO PARA LISTAR AS CONTAS EXISTENTES
+def listarContas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência: \t{conta['agencia']}
+            C/C:\t{conta['numeroDaConta']}
+            Titular:\t{conta['usuário']['nome']}
+
+"""
+        print("="*100)
+        print(linha)
+## FUNÇÃO PRINCIPAL QUE CHAMARÁ TODAS AS FUNÇÕES CRIADAS ACIMA
